1771420f5c72e2e1dd48d17820db1ff3394b6c83
Cedrickpng <cedrick.burudi@student.moringaschool.com>
2025-03-11 20:12:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = `bob`;
+
+// function to uppercase customerName
+function upperCaseCustomerName() {
+  return customerName = `BOB`;
+}  
+
+// Function to declare bestCustomer globally
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer globally
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'Alice';
+
+// Attempting to change 
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = `anybody`;
+}
